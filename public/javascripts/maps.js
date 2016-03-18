504ae33ac30220376ddcdf1447cb9b7914716b67
cjyueh <cjyueh@gmail.com>
2016-03-17 17:48:27 -0700

    begins styling of bar modals on map

--- public/javascripts/maps.js
+++ public/javascripts/maps.js
@@ -61,7 +61,7 @@ function getBarInfo () {
       function renderBarInfo (marker, name, address, pic) {
         google.maps.event.addListener(marker, 'click', function(e) {
             infowindow.open(map, marker);
-            infowindow.setContent("<div style='width:180px;height:150px;'>"+ name + '<br>' + address + '<br>' + "<a href=" + yelp + " target='_blank'>" + "Website</a>" + '<br>' + "<img class='g-pic' src=" + pic + "></div>");
+            infowindow.setContent("<div style='width:200px;height:175px;'><h4>"+ name + '</h4><p>' + address + '</p>' + "<a href=" + yelp + " target='_blank'>" + "Website</a>" + '<br>' + "<img class='g-pic' src=" + pic + "></div>");
         });
       }
       renderBarInfo(marker, name, address, pic);
