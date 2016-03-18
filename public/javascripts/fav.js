e2b856e9384f120b3b3c86c974799a128c15c26a
dchiamp <derek.chiampas@gmail.com>
2016-03-18 01:05:30 -0700

    cleans

--- public/javascripts/fav.js
+++ public/javascripts/fav.js
@@ -12,19 +12,15 @@ var favEndPoint = '/teams/api/' + teamId + '/user';
   e.preventDefault();
   $.ajax({
     type: "PUT",
-    // contentType: "application/json; charset=utf-8",
     url: favEndPoint,
     data: {favorite: teamId},
     dataType: "json",
     success: function (data) {
-      // console.log("this is the data: ",);
       // $favBtn.addClass('btn btn-success');
       $star.removeClass('glyphicon-star-empty');
       $star.addClass('glyphicon-star');
       console.log(data._id)
       var userId = data._id
-
-      // console.log(data)
     },
     error: function (err){
         console.log("error: user not logged in", err);
